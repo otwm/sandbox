bf6d0bfb5a73f21c699fca70fc1fe8992f407c81
dong o kim <otwmoo@gmail.com>
2018-08-17 01:20:52 +0900

    * linux 환경으로 컨버전 * aws build success

--- next-gogo/gulpfile.js
+++ next-gogo/gulpfile.js
@@ -10,18 +10,23 @@ const { log } = console;
 
 const clean = () => del(['./build ', './.next']);
 
-const mvNext = () => gulp.src('./.next/**/*')
+const cpNext = () => gulp.src('./.next/**/*')
     .pipe(gulp.dest('./build/.next'));
-const mvConfig = () => gulp.src(['package.json', 'package-lock.json'])
+const cpConfig = () => gulp.src(['package.json', 'package-lock.json'])
     .pipe(gulp.dest('./build'));
+const cpStatic = () => gulp.src('./static/**/*')
+    .pipe(gulp.dest('./build/static'));
+const cpPages = () => gulp.src('./pages/**/*')
+    .pipe(gulp.dest('./build/pages'));
 
 const archive = () => gulp.src(['./build/**'], {dot: true})
     .pipe(zip(`dev-next-gogo-${moment().format('YYYY-MM-DD-hh_mm_ss')}.zip`))
     .pipe(gulp.dest('./build'));
 
-gulp.task('mvNext', mvNext);
-gulp.task('mvConfig', mvConfig);
-gulp.task('mv', ['mvNext', 'mvConfig']);
+gulp.task('cpNext', cpNext);
+gulp.task('cpConfig', cpConfig);
+gulp.task('cpStatic', cpStatic);
+gulp.task('mv', ['cpNext', 'cpConfig', 'cpStatic']);
 gulp.task('archive', archive);
 gulp.task('ls', done => {
     log(ls('./build'));
