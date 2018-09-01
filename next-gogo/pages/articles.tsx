838e915313fcc0d5e5636f0a23282d6f3f7912f9
dong o kim <otwmoo@gmail.com>
2018-09-01 18:28:50 +0900

    * temp

--- next-gogo/pages/articles.tsx
+++ next-gogo/pages/articles.tsx
@@ -1,23 +1,16 @@
 import {Component} from 'react';
-import {observer} from 'mobx-react';
+import {observer, Provider} from 'mobx-react';
+import ArticleStore, { Article } from '../stores/ArticleStore';
 
 //import ArticleList from '../components/article/ArticleList';
 
-interface IArticle {
-    id: number,
-    title: string,
-    content: string,
-    writer: string,
-    modifier?: string,
-    regDate: Date,
-    modDate?: Date,
-}
-
 interface IArticlesProps {
-    articles: Array<IArticle>,
+    articleStore: ArticleStore;
 }
 
-const getArticles = () => ([
+const articleStore = new ArticleStore({});
+
+const getArticlesData = () => ([
     {
         id: 1,
         title: '새글',
@@ -34,42 +27,45 @@ const getArticles = () => ([
     }
 ]);
 
+//init
+articleStore.addAll(getArticlesData().map(item => new Article(item)));
+
 @observer
 class Articles extends Component<IArticlesProps> {
     static async getInitialProps(): Promise<IArticlesProps> {
-        const result = {
-            articles: getArticles(),
-        };
+        const result = { articleStore };
         return result;
     }
 
     render() {
-        const {articles} = this.props;
+        const { articleStore } = this.props;
         return (
-            <div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>no</th>
-                        <th>title</th>
-                        <th>write date</th>
-                        <th>moifier</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {articles.map(article => {
-                        return (
-                            <tr>
-                                <td>{article.id}</td>
-                                <td>{article.title}</td>
-                                <td>{article.modDate}</td>
-                                <td>{article.modifier}</td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
-            </div>
+            <Provider articleStore={articleStore} >
+                <div>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>no</th>
+                            <th>title</th>
+                            <th>write date</th>
+                            <th>moifier</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {articleStore.articles.map(article => {
+                            return (
+                                <tr key={article.data.id}>
+                                    <td>{article.data.id}</td>
+                                    <td>{article.data.title}</td>
+                                    <td>{article.data.modDate}</td>
+                                    <td>{article.data.modifier}</td>
+                                </tr>
+                            );
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </Provider>
         );
     }
 }
---
+++ next-gogo/stores/ArticleStore.ts
@@ -0,0 +1,88 @@
+import {observable} from 'mobx';
+
+const defTransportLayer = {
+    onReceiveArticleUpdate(values){
+        console.log(values);
+    }
+};
+
+interface IRecord{
+    synchronized: boolean;
+    data: any;
+}
+
+class ArticleStore {
+    transportLayer;
+    count = 0;
+    @observable articles:Array<IRecord> = [];
+    deleteArticles: Array<IRecord> = [];
+
+    constructor({ query = null, transportLayer = defTransportLayer}) {
+        this.transportLayer = transportLayer;
+        this.transportLayer.onReceiveArticleUpdate(articles => this.updateArticleFromServer(articles));
+
+        if ( query != null ) {
+            this.loadArticle(query);
+        }
+    }
+
+    updateArticleFromServer(articles){
+        console.log(articles);
+    }
+
+    loadArticle(query){
+        console.log(query);
+    }
+
+    add(article){
+        this.articles.push(article);
+        return article;
+    }
+
+    addAll(articles){
+        console.log(articles)
+        this.articles = this.articles.concat(articles.map(item => ({ synchronized : false, data:item })));
+    }
+
+    plush(){
+        this.transportLayer.batchUpdate(
+            this.articles.filter(article => !article.synchronized)
+                .map(article => article.data)
+        );
+        this.deleteArticles.push(this.transportLayer.removeAll(this.deleteArticles));
+    }
+
+    removeArticle(id){
+        const index = this.articles.findIndex(article => article.data.id === id);
+        const deletedArticle = this.articles.splice(index, 1)[0];
+        deletedArticle.synchronized = false;
+        this.deleteArticles.push(deletedArticle);
+    }
+}
+
+export class Article {
+    id: number;
+    @observable title: string;
+    @observable content: string;
+    @observable writer: string;
+    @observable modifier: string;
+    @observable regDate: Date;
+    @observable modDate: Date;
+
+    constructor({title, content, writer, regDate}) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.regDate = regDate;
+    }
+
+    update(article){
+        console.log(article);
+    }
+
+    remove(){
+
+    }
+}
+
+export default ArticleStore;
