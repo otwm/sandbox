a5bed5a82b0ef70e9e0210d0933591c0cb60f9b2
dong o kim <otwmoo@gmail.com>
2018-09-30 12:08:52 +0900

    * typestyle

--- next-gogo/pages/index.tsx
+++ next-gogo/pages/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import { style } from 'typestyle';
 
+const className = style({color: 'red'});
 export default () => (
     <div>
-      ok test
+        <div className={className}>ok test</div>
       <ul>
         <li><Link href='/a' as='/a'><a>a</a></Link></li>
         <li><Link href='/b' as='/b'><a>b</a></Link></li>
       </ul>
     </div>
-)
+);
